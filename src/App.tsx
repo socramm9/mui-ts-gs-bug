be76be902d41e048678e3176e4171e40bfdf3605
socramm9 <no-mail@mail.xyz>
2021-12-03 23:11:00 +0100

    mui bug

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import GlobalStyles from "@mui/material/GlobalStyles";
+
+// DOES NOT WORK
+function App() {
+  return (
+    <GlobalStyles
+      styles={(theme: any) => {
+        return {
+          body: {
+            backgroundColor: theme.palette.primary.main,
+          },
+        };
+      }}
+    />
+  );
+}
+// WORKS
+/* function App() {
+  return (
+    <GlobalStyles
+      styles={(theme: any) => {
+        return {
+          body: {
+            backgroundColor: theme.palette.primary.main,
+          },
+        };
+      }}
+    />
+  );
+} */
+
+export default App;
